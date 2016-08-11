5422b9d86bd3d64d5090f90413b44e593d4d39a1
Owner <Owner@MR15-109.local>
2016-08-11 15:55:19 +0900

    day4

--- app/src/main/java/com/lifeistech/android/quiz/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/quiz/MainActivity.java
@@ -71,13 +71,27 @@ public class MainActivity extends ActionBarActivity {
         Question q1 = new Question(R.drawable.japan,"日本の首都は？（腕試し！）","東京","京都","大阪");
         Question q2 = new Question(R.drawable.touhou2,"東方ploject第１２回（最新）の人妖部門での第一位は誰でしょう？","博麗　霊夢","霧雨　魔理沙","フランドール　スカーレット");
         Question q3 = new Question(R.drawable.touhou3,"では、音楽部門での一位の曲は、次のうちどれでしょう？","U.N オーエンは彼女なのか？","亡き王女のためのセプテット","優雅に咲かせ、墨染の桜 〜Border of life");
-        Question q4 = new Question(R.drawable.touhou4,"東方紅魔郷３面ボス・紅 美鈴の（音楽）テーマは？","明治１７年の上海アリス","明治１６年の上海アリス","明治１８年の上海アリス");
+        Question q4 = new Question(R.drawable.touhou4,"東方紅魔郷３面ボスの（音楽）テーマは？","明治１７年の上海アリス","明治１６年の上海アリス","明治１８年の上海アリス");
         Question q5 = new Question(R.drawable.touhou5,"おまけ　( ﾟ∀ﾟ)o彡ﾟ←誰に向かってやってる？","え〜りん","衣玖サン","レミリア　スカーレット");
         Question q6 = new Question(R.drawable.touhou6,"『あなたが、コンティニュー出来ないのさ！』　この台詞のキャラは？","フランドール　スカーレット","レミリア　スカーレット","チルノ");
         Question q7 = new Question(R.drawable.touhou7,"『あなたは今まで食べてきたパンの枚数を覚えてるの？』　この台詞のキャラは？","レミリア　スカーレット","博麗　霊夢","西行寺　幽々子");
         Question q8 = new Question(R.drawable.touhou8,"『一つや二つ・・・ 結界は、そんなに少ないと思って？』　この台詞のキャラは？","八雲　紫","八雲　藍","西行寺　幽々子");
         Question q9 = new Question(R.drawable.touhou9,"永夜抄4面ボス霧雨魔理沙のﾃｰﾏは？","恋色マスタースパーク","魔女達の舞踏会","メイガスナイト");
-//　　　　 Question q10 = new Question(R.drawable.touhou10,"幻想万華鏡の","","","");
+        Question q10 = new Question(R.drawable.touhou10,"「広有射怪鳥事」をひらがなで表すと？","ひろありけちょうをいること","こうゆうしゃかいちょうじ","きれぬものなどあんまりない！");
+        Question q11 = new Question(R.drawable.touhou11,"「幼心地の有頂天」←なんと読む？　（緋想天ラストスペルテーマ）","おさなごこちのうちょうてん","おさなごこちのゆうちょうてん","ようしんちのうちょうてん");
+        Question q12 = new Question(R.drawable.touhou12,"最新のスペカ人気投票の第一位はどれでしょう？","恋符　マスタースパーク","反魂蝶","禁忌　レーヴァテイン");
+        Question q13 = new Question(R.drawable.touhou13,"リリース順に並び替えて、適当な物を選びなさい。","東方永夜紗ー東方華映塚ー東方風神録","東方永夜抄ー東方風神録ー東方花映塚","東方花映塚ー東方永夜抄ー東方風神録");
+        Question q14 = new Question(R.drawable.touhou14,"「紅　美鈴」なんと読む？","ほん　めいりん","くれない　めいりん","こう　めーりん");
+        Question q15 = new Question(R.drawable.touhou15,"特別問題！　製作者の好きなキャラは？","霧雨　魔理沙","フランドール　スカーレット","ケンシロウ");
+        Question q16 = new Question(R.drawable.sazaesann,"実際に、長谷川町子によって描かれたタラちゃんの妹の名前は？","ヒトデ","ホタテ","イルカ");
+        Question q17 = new Question(R.drawable.benntou,"肖像画のベートーベンが不機嫌な理由は？","家政婦の料理がまずかったため。","寝不足のため。","そんな気分じゃなかったため");
+//        Question q18 = new Question(R.drawable.touhou18,"母の日に贈る花で有名な「カーネーション」。 さて、黄色のカーネーションの花言葉は次のうちどれ？
+//        Question q19 = new Question(R.drawable.touhou19,"","","","");
+//        Question q20 = new Question(R.drawable.touhou20,"","","","");
+
+
+
+
         question_list.add(q1);
         question_list.add(q2);
         question_list.add(q3);
@@ -87,8 +101,17 @@ public class MainActivity extends ActionBarActivity {
         question_list.add(q7);
         question_list.add(q8);
         question_list.add(q9);
-
-
+        question_list.add(q10);
+        question_list.add(q11);
+        question_list.add(q12);
+        question_list.add(q13);
+        question_list.add(q14);
+        question_list.add(q15);
+        question_list.add(q16);
+        question_list.add(q17);
+        question_list.add(q18);
+        question_list.add(q19);
+        question_list.add(q20);
         // TODO [03] ここまで
     }
 
